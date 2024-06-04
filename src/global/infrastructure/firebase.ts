bb53d9e531cd8857e087a9e008daf69b4b0e234a
seungryeol <opop0421@naver.com>
2024-06-04 16:14:04 +0900

    [feat] 파이어베이스 알림 전송 api

--- src/global/infrastructure/firebase.ts
+++ src/global/infrastructure/firebase.ts
@@ -1,9 +1,9 @@
 
 import firebaseAdmin from 'firebase-admin';
-import * as googleService from '../docs/google-services.json'
-
+import { createRequire } from 'node:module';
+const require = createRequire(import.meta.url);
+const googleService = require('../docs/firebase-admin.json');
 
 export const firebase = firebaseAdmin.initializeApp({
-
-	credential : firebaseAdmin.credential.cert(googleService as firebaseAdmin.ServiceAccount)
+	credential : firebaseAdmin.credential.cert(googleService)
     });
