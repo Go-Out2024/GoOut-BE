ad2c80be380838da6f70ff07140479d5f04cd632
seungryeol <opop0421@naver.com>
2024-06-06 14:39:53 +0900

    [fix] 파이어베이스 파일 이동

--- src/global/infrastructure/firebase.ts
+++ src/global/infrastructure/firebase.ts
@@ -2,7 +2,7 @@
 import firebaseAdmin from 'firebase-admin';
 import { createRequire } from 'node:module';
 const require = createRequire(import.meta.url);
-const googleService = require('../docs/firebase-admin.json');
+const googleService = require('../../../firebase-admin.json');
 
 export const firebase = firebaseAdmin.initializeApp({
 	credential : firebaseAdmin.credential.cert(googleService)
