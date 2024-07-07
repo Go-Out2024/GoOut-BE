2a17ca061171abd851b492ee834bed2994444d5e
seungryeol <opop0421@naver.com>
2024-07-07 23:33:00 +0900

    [feat] user엔티티 정의

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -6,50 +6,55 @@ import {
     OneToOne,
     OneToMany,
     JoinColumn,
+    Index,
 } from "typeorm"
 import { InternalServerError } from "routing-controllers";
 import { FirebaseToken } from "./FirebaseToken.js"
 import { Relation } from "typeorm";
 
-@Entity("User")
-export class User extends BaseEntity{
+@Entity("user")
+export class User extends BaseEntity {
   
-    constructor(numbers:string, email:string){
+    constructor(numbers:string, email:string) {
         super();
-        this.setNumber(numbers)
-        this.setEmail(email)
+        this.setNumber(numbers);
+        this.setEmail(email);
     }
-
-
 
     @PrimaryGeneratedColumn()
-    id: number
+    id: number;
 
-    @Column()
-    numbers:string;
+    @Column({ type: 'varchar', name: 'numbers' })
+    numbers: string;
 
-    @Column()
+    @Column({ type: 'varchar', name: 'email' })
     email: string;
 
+    @Column({ type: 'boolean', name: 'alarm' })
+    alarm: boolean;
+
+    @Column({ type: 'timestamp', name: 'alarm_start' })
+    alarmStart: Date;
+
+    @Column({ type: 'timestamp', name: 'alarm_end' })
+    alarmEnd: Date;
+
     @OneToMany(() => FirebaseToken, token => token.user)
-    firebaseTokens: Relation<FirebaseToken>[]
-   
-    public static createUser(numbers:string, email:string){
-        return new User(numbers, email)
-    }
-    // 외부에서 쉽게 'User' 인스턴스 생성 가능
+    firebaseTokens: Relation<FirebaseToken>[];
 
-    private setNumber(numbers:string): void{
-        if(numbers === null) throw new InternalServerError(`${__dirname} : nickname 값이 존재하지 않습니다.`);
-        this.numbers=numbers
+    public static createUser(numbers:string, email:string) {
+        return new User(numbers, email);
     }
-    // 유효성 검증
 
-    private setEmail(email:string): void{
-        if(email=== null) throw new InternalServerError(`${__dirname} : profileImage 값이 존재하지 않습니다.`);
-        this.email=email
+    private setNumber(numbers: string): void {
+        if (numbers === null) throw new InternalServerError(`${__dirname} : numbers 값이 존재하지 않습니다.`);
+        this.numbers = numbers;
     }
-        // 유효성 검증
+
+    private setEmail(email: string): void {
+        if (email === null) throw new InternalServerError(`${__dirname} : email 값이 존재하지 않습니다.`);
+        this.email = email;
+    }
 
     public getNumber() {
         return this.numbers;
@@ -58,5 +63,4 @@ export class User extends BaseEntity{
     public getEmail() {
         return this.email;
     }
-
 }
