08b05ceaf33e91279d79443db1fa1db057dcc7bb
taesoek <xotjr06@gmail.com>
2024-07-22 23:55:06 +0900

    [fix] 에러 코드 변경

--- src/exception/ErrorCode.ts
+++ src/exception/ErrorCode.ts
@@ -5,7 +5,7 @@ export enum ErrorCode  {
     ERROR = 501,
     NO_VALUE=502,
     NOT_FOUND_EATERY_CATEGORY=400,
-    NOT_FOUND_STATION_NAME=401
+    NOT_FOUND_STATION_NAME=402
 }
 
 /** 각 에러 코드에 대한 메시지 정의 */
@@ -13,7 +13,7 @@ const errorMessages: { [key: number]: string } = {
     501: "강제 에러 발생",
     502: "해당 값이 존재하지 않습니다.",
     400: "해당 카테고리는 존재하지 않습니다.",
-    401: "해당 역은 존재하지 않습니다."
+    402: "해당 역은 존재하지 않습니다."
 };
 
 export function errorMessage(code: ErrorCode): string {
