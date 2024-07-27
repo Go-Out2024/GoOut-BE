fea513a14fff66cca203460603a8516384af1555
seungryeol <opop0421@naver.com>
2024-07-28 01:58:38 +0900

    [fix] 스케줄러 시간 설정

--- src/util/enum/scheduler.ts
+++ src/util/enum/scheduler.ts
@@ -6,8 +6,8 @@ import { RedisService } from '../../service/Redis.Service.js';
 export const settingRecommendMusic = async () => {
 
     await schedule.scheduleJob(
-       // '*/10 * * * * *'
-       '*/1 * * * *'
+        '*/10 * * * * *'
+     //  '*/1 * * * *'
         , async function () {       // UTC시간 기준 9시간 차이로 새벽 12시 의미
         const result = await musicRecommend();  
         await new RedisService().setValue('today-music', result);
