c1acfac96c5af5b7d9f62d823152d219e1a5d872
seungryeol <opop0421@naver.com>
2024-08-06 23:25:58 +0900

    [add] 환경변수 추가

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -23,6 +23,7 @@ export const envs = {
   prefix: process.env.PREFIX || '',
   apikey:{
     weatherapikey: process.env.WEATHER_API_KEY as string,
-    openai: process.env.OPEN_AI_API_KEY as string
+    openai: process.env.OPEN_AI_API_KEY as string,
+    youtubeapikey: process.env.YOUTUBE_API_KEY as string,
   },
 };
