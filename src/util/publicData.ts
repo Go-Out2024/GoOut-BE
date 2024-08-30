13f8efb3f98feda466e4cdee5cc4ff884d7db724
seungryeol <opop0421@naver.com>
2024-08-30 15:36:55 +0900

    [style] 공백처리

--- src/util/publicData.ts
+++ src/util/publicData.ts
@@ -18,8 +18,6 @@ export class SubwayApi {
             if (data.RESULT && data.RESULT.code !== 'INFO-000') {
                 throw new Error(`Error from API: ${data.RESULT.message}`);
             }
-            console.log(data)
-
             return data.realtimeArrivalList; // 필터링은 호출하는 서비스 함수에서 수행
         } catch (error) {
             console.error('Failed to fetch subway arrival info:', error.message);
