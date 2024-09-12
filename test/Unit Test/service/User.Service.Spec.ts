1ff622c554d3e155fa204ddeecdb950d53a2a4c5
seungryeol <opop0421@naver.com>
2024-09-12 21:58:25 +0900

    [test] user service test

--- test/Unit Test/service/User.Service.Spec.ts
+++ test/Unit Test/service/User.Service.Spec.ts
@@ -95,9 +95,124 @@ describe('User Service Test', ()=>{
         });
     });
 
-    // describe('penetrateFirebaseToken function test', ()=>{
-    //     it('basic',  async () => {
-       
-    //     });
-    // });
+    describe('penetrateFirebaseToken function test', ()=>{
+        const token = 'token'
+        it('basic',  async () => {
+            userRepository.findUserById.mockResolvedValue(user);
+            mockVerifyUser.mockReturnValue(null);
+
+            const result = await userService.penetrateFirebaseToken(userId,token);
+
+            expect(result).toEqual(undefined);
+            expect(userRepository.findUserById).toHaveBeenCalledWith(userId);
+            expect(mockVerifyUser).toHaveBeenCalledWith(user);
+            expect(firebaseTokenRepository.insertToken).toHaveBeenCalledWith(user,token);
+        });
+
+        it('verifyUser error',  async () => {
+            userRepository.findUserById.mockResolvedValue(undefined);
+            mockVerifyUser.mockImplementation(()=>{
+                throw ErrorResponseDto.of(ErrorCode.NOT_FOUND_USER)
+            })
+            await expect(userService.penetrateFirebaseToken(userId,token))
+            .rejects
+            .toEqual(ErrorResponseDto.of(ErrorCode.NOT_FOUND_USER));
+            expect(userRepository.findUserById).toHaveBeenCalledWith(userId);
+            expect(mockVerifyUser).toHaveBeenCalledWith(undefined);
+            expect(firebaseTokenRepository.insertToken).not.toHaveBeenCalledWith(user,token);
+        });
+    });
+
+
+    describe('modifyAlarmOnOff function test', ()=>{
+        const status = true;
+        it('basic',  async () => {
+            userRepository.findUserById.mockResolvedValue(user);
+            mockVerifyUser.mockReturnValue(null);
+
+            const result = await userService.modifyAlarmOnOff(userId,status);
+
+            expect(result).toEqual(undefined);
+            expect(userRepository.findUserById).toHaveBeenCalledWith(userId);
+            expect(mockVerifyUser).toHaveBeenCalledWith(user);
+            expect(userRepository.updateAlarmStatus).toHaveBeenCalledWith(userId,status);
+    
+        });
+
+        it('verifyUser error',  async () => { 
+            userRepository.findUserById.mockResolvedValue(undefined);
+            mockVerifyUser.mockImplementation(()=>{
+                throw ErrorResponseDto.of(ErrorCode.NOT_FOUND_USER)
+            })
+            await expect(userService.modifyAlarmOnOff(userId,status))
+            .rejects
+            .toEqual(ErrorResponseDto.of(ErrorCode.NOT_FOUND_USER));
+            expect(userRepository.findUserById).toHaveBeenCalledWith(userId);
+            expect(mockVerifyUser).toHaveBeenCalledWith(undefined);
+            expect(userRepository.updateAlarmStatus).not.toHaveBeenCalledWith(userId,status);
+        });
+
+    });
+
+    describe('modifyAlarmTime function test', ()=>{
+        
+        const alarmStart = 'alarmStart';
+        const alarmEnd = 'alarmEnd';
+        it('basic',  async () => {
+            userRepository.findUserById.mockResolvedValue(user);
+            mockVerifyUser.mockReturnValue(null);
+
+            const result = await userService.modifyAlarmTime(userId,alarmStart,alarmEnd);
+
+            expect(result).toEqual(undefined);
+            expect(userRepository.findUserById).toHaveBeenCalledWith(userId);
+            expect(mockVerifyUser).toHaveBeenCalledWith(user);
+            expect(userRepository.updateAlarmTime).toHaveBeenCalledWith(userId,alarmStart,alarmEnd);
+    
+        });
+
+        it('verifyUser error',  async () => {
+            userRepository.findUserById.mockResolvedValue(undefined);
+            mockVerifyUser.mockImplementation(()=>{
+                throw ErrorResponseDto.of(ErrorCode.NOT_FOUND_USER)
+            })
+            await expect(userService.modifyAlarmTime(userId,alarmStart,alarmEnd))
+            .rejects
+            .toEqual(ErrorResponseDto.of(ErrorCode.NOT_FOUND_USER));
+            expect(userRepository.findUserById).toHaveBeenCalledWith(userId);
+            expect(mockVerifyUser).toHaveBeenCalledWith(undefined);
+            expect(userRepository.updateAlarmTime).not.toHaveBeenCalledWith(userId,alarmStart,alarmEnd);
+        });
+    });
+
+    describe('eraseUser function test', ()=>{
+        it('basic',  async () => {
+            userRepository.findUserById.mockResolvedValue(user);
+            mockVerifyUser.mockReturnValue(null);
+
+            const result = await userService.eraseUser(userId);
+
+            expect(result).toEqual(undefined);
+            expect(userRepository.findUserById).toHaveBeenCalledWith(userId);
+            expect(mockVerifyUser).toHaveBeenCalledWith(user);
+            expect(userRepository.deleteUser).toHaveBeenCalledWith(userId);
+    
+        });
+
+        it('verifyUser error',  async () => {
+            userRepository.findUserById.mockResolvedValue(undefined);
+            mockVerifyUser.mockImplementation(()=>{
+                throw ErrorResponseDto.of(ErrorCode.NOT_FOUND_USER)
+            })
+            await expect(userService.eraseUser(userId))
+            .rejects
+            .toEqual(ErrorResponseDto.of(ErrorCode.NOT_FOUND_USER));
+            expect(userRepository.findUserById).toHaveBeenCalledWith(userId);
+            expect(mockVerifyUser).toHaveBeenCalledWith(undefined);
+            expect(userRepository.deleteUser).not.toHaveBeenCalledWith(userId);
+        
+        });
+   });
+
+
 });
