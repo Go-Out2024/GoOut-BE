25fab5fc8fd8c16ffc6e32bfd3e172af829fbde4
seungryeol <opop0421@naver.com>
2024-09-19 13:32:14 +0900

    [test] firebaseMessage util test

---
+++ test/Unit Test/util/firebaseMessage.Spec.ts
@@ -0,0 +1,37 @@
+import { pushNotice } from '../../../src/util/firebaseMessage';
+import { firebase } from '../../../src/config/firebase'; 
+
+
+jest.mock('../../../src/config/firebase', () => {
+    const send = jest.fn().mockResolvedValue('Successfully sent message');
+    return {
+        firebase: {
+            messaging: jest.fn().mockReturnValue({
+                send,
+            }),
+        },
+    };
+});
+
+describe('firebaseMessage Util Test', () => {
+    describe('pushNotice function test', () => {
+        it('should send a notification and log success', async () => {
+            const engineValue = 'engineValue';
+            const title = 'title';
+            const body = 'body';
+            const sendSpy = jest.spyOn(firebase.messaging(), 'send');
+            const logSpy = jest.spyOn(console, 'log').mockImplementation(() => {});
+            await pushNotice(engineValue, title, body);
+            expect(sendSpy).toHaveBeenCalledWith({
+                token: engineValue,
+                notification: {
+                    title: title,
+                    body: body,
+                },
+            });
+            expect(logSpy).toHaveBeenCalledWith('Successfully sent message:', 'Successfully sent message');
+            sendSpy.mockRestore();  // 스파이 제거
+            logSpy.mockRestore();
+        });
+    });
+});
