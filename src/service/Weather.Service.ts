cf88a637796aa5bbda20f54ad237f732051d34f8
taesoek <xotjr06@gmail.com>
2024-10-02 18:40:13 +0900

    [fix] 불러오는 날씨 데이터 추가(너무 적으면 최저 최고 기온을 주지 않음.)

--- src/service/Weather.Service.ts
+++ src/service/Weather.Service.ts
@@ -71,7 +71,7 @@ export class WeatherService {
             params: {
                 serviceKey: this.apikey,
                 pageNo: 1,
-                numOfRows: 120,
+                numOfRows: 305,
                 dataType: 'JSON',
                 base_date: baseDate,
                 base_time: baseTime,
