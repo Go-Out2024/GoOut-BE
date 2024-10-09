70405e89eb5082d571f73b09ad360b3d1cece6e0
seungryeol <opop0421@naver.com>
2024-10-09 14:42:15 +0900

    [test] FamousSaying 통합 테스트 완료

---
+++ test/Integration Test/FamousSaying.Api.Spec.ts
@@ -0,0 +1,32 @@
+import { initializeDatabase } from "../../src/config/database";
+import { connectToRedis, redisClient } from "../../src/config/redis";
+import app from "../../src/index";
+import request from "supertest";
+import { Connection } from "typeorm";
+
+let connection: Connection;
+
+describe("Kakao Eatery API", () => {
+  beforeAll(async () => {
+    connection = await initializeDatabase();
+    await connectToRedis();
+  });
+
+  afterAll(async () => {
+    await connection.close();
+    await redisClient.quit();
+  });
+
+  describe("GET /api/famous-saying", () => {
+    it("200", (done) => {
+      request(app)
+        .get("/api/famous-saying")
+        .expect(200)
+        .expect((res) => {
+          expect(res.body).toHaveProperty("data");
+          expect(res.body.data).toBeInstanceOf(Object);
+        })
+        .end(done);
+    });
+  });
+});
