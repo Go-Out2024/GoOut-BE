728ae606ddbeee96bbc75f828a0bb86578829232
seungryeol <opop0421@naver.com>
2024-10-15 12:28:33 +0900

    [fix] 알람 주기 설정

--- src/util/scheduler.ts
+++ src/util/scheduler.ts
@@ -1,39 +1,42 @@
-
-import schedule from 'node-schedule';
-import { YouTubeSchedule } from './YouTubeSchedule';
+import schedule from "node-schedule";
+import { YouTubeSchedule } from "./YouTubeSchedule";
 import { RedisService } from "../service/Redis.Service";
 import { YouTubeApi } from "./YouTubeApi";
-import { AlarmRepository } from '../repository/Alarm.Repository';
-import { Alarm } from './Alarm';
-import {Container} from 'typedi';
+import { AlarmRepository } from "../repository/Alarm.Repository";
+import { Alarm } from "./Alarm";
+import { Container } from "typedi";
 
 export const settingRecommendMusic = async () => {
-
-    await schedule.scheduleJob(
-        '0 0 */3 * *'
+  await schedule.scheduleJob(
+    "0 0 */3 * *",
     //    '0 0 1 0 * *'
-//'*/10 * * * * *'
+    //'*/10 * * * * *'
     //   '*/1 * * * *'
-        , async function () {       // UTC시간 기준 9시간 차이로 새벽 12시 의미 
-            await new YouTubeSchedule(new RedisService, new YouTubeApi).youtubeScheduler();
-    });
-    console.log("노래 추천 스케줄링 완료")
-}
-
+    async function () {
+      // UTC시간 기준 9시간 차이로 새벽 12시 의미
+      await new YouTubeSchedule(
+        new RedisService(),
+        new YouTubeApi()
+      ).youtubeScheduler();
+    }
+  );
+  console.log("노래 추천 스케줄링 완료");
+};
 
 export const handleAlarm = async () => {
-    await schedule.scheduleJob(
-     //   '0 0 0 1 * *'
+  await schedule.scheduleJob(
+    //   '0 0 0 1 * *'
     //'*/10 * * * * *'
-     //  '*/1 * * * *'
-     '0 0 */3 * *'
-        , async function () {       // UTC시간 기준 9시간 차이로 새벽 12시 의미 
-            const datas : any[] = await Container.get(AlarmRepository).findDataForAlarm();
-            const alarmService = Container.get(Alarm);
-            await alarmService.handleAlarm(datas);
-    });
-    console.log("알림 스케줄링 완료")
-}
-
-
-
+    //  '*/1 * * * *'
+    "* * * * *",
+    async function () {
+      // UTC시간 기준 9시간 차이로 새벽 12시 의미
+      const datas: any[] = await Container.get(
+        AlarmRepository
+      ).findDataForAlarm();
+      const alarmService = Container.get(Alarm);
+      await alarmService.handleAlarm(datas);
+    }
+  );
+  console.log("알림 스케줄링 완료");
+};
