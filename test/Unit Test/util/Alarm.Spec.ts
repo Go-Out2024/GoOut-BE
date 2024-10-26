c69cd1d8fb5310c1c381d830f22fb3f49659633b
seungryeol <opop0421@naver.com>
2024-10-26 16:29:57 +0900

    [test] 알림 로직 변경 테스트코드

--- test/Unit Test/util/Alarm.Spec.ts
+++ test/Unit Test/util/Alarm.Spec.ts
@@ -1,153 +1,188 @@
-import {Alarm} from '../../../src/util/Alarm';
-import {TrafficService} from '../../../src/service/Traffic.Service';
-import { pushNotice } from '../../../src/util/firebaseMessage';
-import { TrafficCollection } from '../../../src/entity/TrafficCollection';
-import { BusStationResult, StationResult, SubwayStationResult } from '../../../src/dto/values/StationResult';
+import { Alarm } from "../../../src/util/Alarm";
+import { TrafficService } from "../../../src/service/Traffic.Service";
+import { pushNotice } from "../../../src/util/firebaseMessage";
+import { TrafficCollection } from "../../../src/entity/TrafficCollection";
+import {
+  BusStationResult,
+  StationResult,
+  SubwayStationResult,
+} from "../../../src/dto/values/StationResult";
 
+jest.mock("../../../src/service/Traffic.Service");
+jest.mock("../../../src/util/firebaseMessage");
 
-jest.mock('../../../src/service/Traffic.Service')
-jest.mock('../../../src/util/firebaseMessage');
+describe("Alarm Util Test", () => {
+  beforeAll(async () => {});
 
+  afterAll(async () => {});
 
-describe('Alarm Util Test', ()=>{
+  let alarm: Alarm;
+  const trafficService = new TrafficService(
+    {} as any,
+    {} as any,
+    {} as any,
+    {} as any,
+    {} as any,
+    {} as any,
+    {} as any,
+    {} as any
+  ) as jest.Mocked<TrafficService>;
 
-    beforeAll(async()=>{
-    })
+  beforeEach(async () => {
+    alarm = new Alarm(trafficService);
+    jest.clearAllMocks();
+  });
 
-    afterAll(async()=>{
+  afterEach(async () => {
+    jest.resetAllMocks();
+  });
+
+  describe("handleAlarm function test", () => {
+    it("basic", async () => {
+      const transportationData = {
+        collection: {} as TrafficCollection,
+        result: {} as StationResult,
+      };
+      const arrivalDatas = ["10분 후 도착"];
+
+      const datas = [
+        { token: "token1", user_id: 1 },
+        { token: "token2", user_id: 2 },
+      ];
+      trafficService.bringMainTrafficCollection.mockResolvedValue(
+        transportationData
+      );
+      const arrivalDatasSpy = jest
+        .spyOn(alarm as any, "extractTransportationArrivalInfo")
+        .mockReturnValue(arrivalDatas);
+      const flagSpy = jest
+        .spyOn(alarm as any, "checkTransportationTime")
+        .mockReturnValue(["true 10분 (회기)"]);
+      const sendPushAlarmSpy = jest.spyOn(alarm as any, "sendPushAlarm");
+
+      await alarm.handleAlarm(datas);
+
+      expect(trafficService.bringMainTrafficCollection).toHaveBeenCalledWith(
+        datas[0].user_id
+      );
+      expect(arrivalDatasSpy).toHaveBeenCalledWith(transportationData.result);
+      expect(flagSpy).toHaveBeenCalledWith(arrivalDatas);
+      expect(sendPushAlarmSpy).toHaveBeenCalledWith(datas[0].token, [
+        "true 10분 (회기)",
+      ]);
+    });
+  });
+
+  describe("sendPushAlarm function test", () => {
+    const flagDatas = ["true 10분 (회기)"];
+    it("basic", async () => {
+      await alarm["sendPushAlarm"]("engineValue", flagDatas);
+      expect(pushNotice).toHaveBeenCalledWith(
+        "engineValue",
+        "교통수단 알림",
+        "등록하신 교통수단의 도착 시간이 (회기)역방면 10분 전입니다."
+      );
+    });
+  });
+
+  describe("checkTransportationTime function test", () => {
+    const time = ["10분 ", "[5]번째"];
+    const flagDatas = ["true 10분 ", "true [5]번째 "];
+    it("basic", () => {
+      const result = alarm["checkTransportationTime"](time);
+      expect(result).toEqual(flagDatas);
+    });
+  });
+
+  describe("checkTransportationTime function test", () => {
+    it("getBusStations undefined", () => {
+      const stationResult = {
+        getSubwayStation: jest.fn().mockReturnValue({
+          getSubwayArrivalInfo: jest
+            .fn()
+            .mockReturnValue(["result1", "result2"]),
+        }),
+        getBusStations: jest.fn().mockReturnValue(undefined),
+      } as unknown as StationResult;
+      const extractSubwayArrivalInfoSpy = jest
+        .spyOn(alarm as any, "extractSubwayArrivalInfo")
+        .mockReturnValue(["result1", "result2"]);
+      const result = alarm["extractTransportationArrivalInfo"](stationResult);
+      expect(result).toEqual(["result1", "result2"]);
+      expect(extractSubwayArrivalInfoSpy).toHaveBeenCalledWith(
+        stationResult.getSubwayStation()
+      );
     });
 
-    let alarm : Alarm;
-    const trafficService = new TrafficService({} as any,{} as any,{} as any,{} as any,{} as any,{} as any,{} as any,{} as any) as jest.Mocked<TrafficService>
-
-    beforeEach(async () => {
-        alarm = new Alarm(trafficService);
-        jest.clearAllMocks();
+    it("getBusStations undefined", () => {
+      const stationResult = {
+        getSubwayStation: jest.fn().mockReturnValue(undefined),
+        getBusStations: jest.fn().mockReturnValue({
+          getBusArrivalInfo: jest.fn().mockReturnValue(["result1", "result2"]),
+        }),
+      } as unknown as StationResult;
+      const extractSubwayArrivalInfoSpy = jest
+        .spyOn(alarm as any, "extractBusArrivalInfo")
+        .mockReturnValue(["result1", "result2"]);
+      const result = alarm["extractTransportationArrivalInfo"](stationResult);
+      expect(result).toEqual(["result1", "result2"]);
+      expect(extractSubwayArrivalInfoSpy).toHaveBeenCalledWith(
+        stationResult.getBusStations()
+      );
     });
 
-    afterEach(async () => {
-        jest.resetAllMocks();
+    it("all undefined", () => {
+      const stationResult = {
+        getSubwayStation: jest.fn().mockReturnValue(undefined),
+        getBusStations: jest.fn().mockReturnValue(undefined),
+      } as unknown as StationResult;
+
+      const result = alarm["extractTransportationArrivalInfo"](stationResult);
+      expect(result).toBeUndefined();
     });
+  });
 
-    describe('handleAlarm function test', ()=>{
-
-        it('basic', async ()=>{
-            const transportationData = {
-                collection: {} as TrafficCollection,
-                result: {} as  StationResult
-            };
-            const arrivalDatas = ['10분 후 도착'];
-
-            const datas = [{token:'token1', user_id: 1},{token:'token2', user_id: 2} ]
-            trafficService.bringMainTrafficCollection.mockResolvedValue(transportationData);
-            const arrivalDatasSpy = jest.spyOn(alarm as any, 'extractTransportationArrivalInfo').mockReturnValue(arrivalDatas);
-            const flagSpy = jest.spyOn(alarm as any, 'checkTransportationTime').mockReturnValue(true);
-            const sendPushAlarmSpy = jest.spyOn(alarm as any, 'sendPushAlarm');
-
-            await alarm.handleAlarm(datas);
-
-            expect(trafficService.bringMainTrafficCollection).toHaveBeenCalledWith(datas[0].user_id);
-            expect(arrivalDatasSpy).toHaveBeenCalledWith(transportationData.result);
-            expect(flagSpy).toHaveBeenCalledWith(arrivalDatas);
-            expect(sendPushAlarmSpy).toHaveBeenCalledWith(datas[0].token, true);
-        });
+  describe("extractSubwayArrivalInfo function test", () => {
+    it("basic", () => {
+      const subwayStationMock = {
+        getSubwayArrivalInfo: jest.fn().mockReturnValue([
+          { getFirstArrivalMessage: jest.fn().mockReturnValue("5번째 도착") },
+          {
+            getFirstArrivalMessage: jest.fn().mockReturnValue("10분 후 도착"),
+          },
+        ]),
+      } as unknown as SubwayStationResult;
+      const result = alarm["extractSubwayArrivalInfo"](subwayStationMock);
+      expect(result).toEqual(["5번째 도착", "10분 후 도착"]);
+      expect(subwayStationMock.getSubwayArrivalInfo).toHaveBeenCalled();
     });
+  });
 
-    describe('sendPushAlarm function test', ()=>{
-        const flag = true;
-        it('basic', async ()=>{
+  describe("extractBusArrivalInfo function test", () => {
+    it("basic", () => {
+      const busStationMock = {
+        getBusStationsInfo: jest.fn().mockReturnValue([
+          {
+            getBusArrivalInfo: jest
+              .fn()
+              .mockReturnValue([
+                { getArrmsg1: jest.fn().mockReturnValue("버스 1 도착") },
+                { getArrmsg1: jest.fn().mockReturnValue("버스 2 도착") },
+              ]),
+          },
+          {
+            getBusArrivalInfo: jest
+              .fn()
+              .mockReturnValue([
+                { getArrmsg1: jest.fn().mockReturnValue("버스 3 도착") },
+              ]),
+          },
+        ]),
+      } as unknown as BusStationResult;
+      const result = alarm["extractBusArrivalInfo"](busStationMock);
 
-            await alarm['sendPushAlarm']('engineValue', flag);
-            expect(pushNotice).toHaveBeenCalledWith('engineValue', "교통수단 알림", "등록하신 교통수단의 도착 시간이 10분 전입니다.")
-        });
+      expect(result).toEqual(["버스 1 도착", "버스 2 도착", "버스 3 도착"]);
+      expect(busStationMock.getBusStationsInfo).toHaveBeenCalled();
     });
-
-
-    describe('checkTransportationTime function test', ()=>{
-        const time =  ['10분 후 도착'];
-        it('basic', ()=>{
-            const result = alarm['checkTransportationTime'](time);
-            expect(result).toEqual(true)
-        });
-    });
-
-    describe('checkTransportationTime function test', ()=>{
-
-        it('getBusStations undefined', ()=>{
-            const stationResult = {
-                getSubwayStation: jest.fn().mockReturnValue({
-                    getSubwayArrivalInfo: jest.fn().mockReturnValue(['result1', 'result2']),
-                }),
-                getBusStations: jest.fn().mockReturnValue(undefined),
-            } as unknown as StationResult;
-            const extractSubwayArrivalInfoSpy = jest.spyOn(alarm as any, 'extractSubwayArrivalInfo').mockReturnValue(['result1', 'result2']);
-            const result = alarm['extractTransportationArrivalInfo'](stationResult);
-            expect(result).toEqual(['result1', 'result2']);
-            expect(extractSubwayArrivalInfoSpy).toHaveBeenCalledWith(stationResult.getSubwayStation());
-        });
-
-        it('getBusStations undefined', ()=>{
-            const stationResult = {
-                getSubwayStation: jest.fn().mockReturnValue(undefined),
-                getBusStations: jest.fn().mockReturnValue({
-                    getBusArrivalInfo: jest.fn().mockReturnValue(['result1', 'result2']),
-                }),
-            } as unknown as StationResult;
-            const extractSubwayArrivalInfoSpy = jest.spyOn(alarm as any, 'extractBusArrivalInfo').mockReturnValue(['result1', 'result2']);
-            const result = alarm['extractTransportationArrivalInfo'](stationResult);
-            expect(result).toEqual(['result1', 'result2']);
-            expect(extractSubwayArrivalInfoSpy).toHaveBeenCalledWith(stationResult.getBusStations());
-        });
-
-        it('all undefined', ()=>{
-            const stationResult = {
-                getSubwayStation: jest.fn().mockReturnValue(undefined),
-                getBusStations: jest.fn().mockReturnValue(undefined),
-            } as unknown as StationResult;
-          
-            const result = alarm['extractTransportationArrivalInfo'](stationResult);
-            expect(result).toBeUndefined()
-        });
-    });
-
-
-    describe('extractSubwayArrivalInfo function test', ()=>{
- 
-        it('basic', ()=>{
-            const subwayStationMock = {
-                getSubwayArrivalInfo: jest.fn().mockReturnValue([
-                    { getFirstArrivalMessage: jest.fn().mockReturnValue('5번째 도착') },
-                    { getFirstArrivalMessage: jest.fn().mockReturnValue('10분 후 도착') },
-                ]),
-            } as unknown as SubwayStationResult;
-            const result = alarm['extractSubwayArrivalInfo'](subwayStationMock);
-            expect(result).toEqual(['5번째 도착', '10분 후 도착']);
-            expect(subwayStationMock.getSubwayArrivalInfo).toHaveBeenCalled();
-        });
-    });
-
-    describe('extractBusArrivalInfo function test', ()=>{
- 
-        it('basic', ()=>{
-            const busStationMock = {
-                getBusStationsInfo: jest.fn().mockReturnValue([
-                    {
-                        getBusArrivalInfo: jest.fn().mockReturnValue([
-                            { getArrmsg1: jest.fn().mockReturnValue('버스 1 도착') },
-                            { getArrmsg1: jest.fn().mockReturnValue('버스 2 도착') },
-                        ]),
-                    },
-                    {
-                        getBusArrivalInfo: jest.fn().mockReturnValue([
-                            { getArrmsg1: jest.fn().mockReturnValue('버스 3 도착') },
-                        ]),
-                    },
-                ]),
-            } as unknown as BusStationResult;
-            const result = alarm['extractBusArrivalInfo'](busStationMock);
-
-            expect(result).toEqual(['버스 1 도착', '버스 2 도착', '버스 3 도착']);
-            expect(busStationMock.getBusStationsInfo).toHaveBeenCalled();
-        });
-    });
+  });
 });
--- test/Unit Test/util/scheduler.Spec.ts
+++ test/Unit Test/util/scheduler.Spec.ts
@@ -1,62 +1,77 @@
-import schedule from 'node-schedule';
-import { YouTubeSchedule } from '../../../src/util/YouTubeSchedule';
+import schedule from "node-schedule";
+import { YouTubeSchedule } from "../../../src/util/YouTubeSchedule";
 import { RedisService } from "../../../src/service/Redis.Service";
 import { YouTubeApi } from "../../../src/util/YouTubeApi";
-import { AlarmRepository } from '../../../src/repository/Alarm.Repository';
-import { Alarm } from '../../../src/util/Alarm';
-import { Container } from 'typedi';
-import {settingRecommendMusic,handleAlarm }   from '../../../src/util/scheduler';
+import { AlarmRepository } from "../../../src/repository/Alarm.Repository";
+import { Alarm } from "../../../src/util/Alarm";
+import { Container } from "typedi";
+import {
+  settingRecommendMusic,
+  handleAlarm,
+} from "../../../src/util/scheduler";
 
-jest.mock('node-schedule');
-jest.mock('../../../src/util/YouTubeSchedule');
-jest.mock('../../../src/service/Redis.Service');
-jest.mock('../../../src/util/YouTubeApi');
-jest.mock('../../../src/repository/Alarm.Repository');
+jest.mock("node-schedule");
+jest.mock("../../../src/util/YouTubeSchedule");
+jest.mock("../../../src/service/Redis.Service");
+jest.mock("../../../src/util/YouTubeApi");
+jest.mock("../../../src/repository/Alarm.Repository");
 
-describe('scheduler Util Test', () => {
-
-    beforeEach(() => {
-        jest.clearAllMocks();
-        Container.set(RedisService, new RedisService());
-        Container.set(YouTubeApi, new YouTubeApi());
-        Container.set(AlarmRepository, {
-            findDataForAlarm: jest.fn().mockResolvedValue([{ user_id: 1, token: 'test-token' }])
-        });
-        Container.set(Alarm, {
-            handleAlarm: jest.fn()
-        });
+describe("scheduler Util Test", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    Container.set(RedisService, new RedisService());
+    Container.set(YouTubeApi, new YouTubeApi());
+    Container.set(AlarmRepository, {
+      findDataForAlarm: jest
+        .fn()
+        .mockResolvedValue([{ user_id: 1, token: "test-token" }]),
     });
-
-    describe('settingRecommendMusic function test', () => {
-        it('should schedule the YouTube music recommendation', async () => {
-            const scheduleJobSpy = jest.spyOn(schedule, 'scheduleJob').mockImplementation((cron, func) => {
-                func();
-                return {};
-            });
-
-            const youtubeSchedulerMock = jest.fn();
-            YouTubeSchedule.prototype.youtubeScheduler = youtubeSchedulerMock;
-
-      
-            await settingRecommendMusic();
-
-            expect(scheduleJobSpy).toHaveBeenCalledWith('0 0 */3 * *', expect.any(Function));
-            expect(youtubeSchedulerMock).toHaveBeenCalled();
-        });
+    Container.set(Alarm, {
+      handleAlarm: jest.fn(),
     });
+  });
 
-    describe('handleAlarm function test', () => {
-        it('should schedule the alarm handling', async () => {
-            const scheduleJobSpy = jest.spyOn(schedule, 'scheduleJob').mockImplementation((cron, func) => {
-                func();
-                return {};
-            });
-
-            const alarmServiceMock = Container.get(Alarm);
-
-            await handleAlarm();
-            expect(scheduleJobSpy).toHaveBeenCalledWith('0 0 */3 * *', expect.any(Function));
-            expect(alarmServiceMock.handleAlarm).toHaveBeenCalledWith([{ user_id: 1, token: 'test-token' }]);
+  describe("settingRecommendMusic function test", () => {
+    it("should schedule the YouTube music recommendation", async () => {
+      const scheduleJobSpy = jest
+        .spyOn(schedule, "scheduleJob")
+        .mockImplementation((cron, func) => {
+          func();
+          return {};
         });
+
+      const youtubeSchedulerMock = jest.fn();
+      YouTubeSchedule.prototype.youtubeScheduler = youtubeSchedulerMock;
+
+      await settingRecommendMusic();
+
+      expect(scheduleJobSpy).toHaveBeenCalledWith(
+        "0 0 */3 * *",
+        expect.any(Function)
+      );
+      expect(youtubeSchedulerMock).toHaveBeenCalled();
     });
+  });
+
+  describe("handleAlarm function test", () => {
+    it("should schedule the alarm handling", async () => {
+      const scheduleJobSpy = jest
+        .spyOn(schedule, "scheduleJob")
+        .mockImplementation((cron, func) => {
+          func();
+          return {};
+        });
+
+      const alarmServiceMock = Container.get(Alarm);
+
+      await handleAlarm();
+      expect(scheduleJobSpy).toHaveBeenCalledWith(
+        "* * * */5 *",
+        expect.any(Function)
+      );
+      expect(alarmServiceMock.handleAlarm).toHaveBeenCalledWith([
+        { user_id: 1, token: "test-token" },
+      ]);
+    });
+  });
 });
