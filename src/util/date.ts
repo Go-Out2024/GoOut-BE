f9ebecb4e3139e4a9c22da080a279781ce729035
seungryeol <opop0421@naver.com>
2024-11-01 15:03:50 +0900

    [feat] 데이트 함수

--- src/util/date.ts
+++ src/util/date.ts
@@ -2,10 +2,18 @@ export const amCheck = () => {
   const now = new Date();
   if (
     now.getHours() >= 5 &&
-    (now.getHours() < 11 || (now.getHours() === 11 && now.getMinutes() <= 59))
+    (now.getHours() < 14 || (now.getHours() === 14 && now.getMinutes() <= 59))
   ) {
     return true;
   } else {
     return false;
   }
+};
+
+export const getTodayDate = () => {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = String(today.getMonth() + 1).padStart(2, "0");
+  const day = String(today.getDate()).padStart(2, "0");
+  return `${year}-${month}-${day}`;
 };
