592b4bcc821fd4e935dd83ef0e8f44b46e121225
seungryeol <opop0421@naver.com>
2024-11-05 16:47:31 +0900

    [feat] date 관련 로직처리 함수

--- src/util/date.ts
+++ src/util/date.ts
@@ -2,7 +2,7 @@ export const amCheck = () => {
   const now = new Date();
   if (
     now.getHours() >= 5 &&
-    (now.getHours() < 14 || (now.getHours() === 14 && now.getMinutes() <= 59))
+    (now.getHours() < 11 || (now.getHours() === 11 && now.getMinutes() <= 59))
   ) {
     return true;
   } else {
