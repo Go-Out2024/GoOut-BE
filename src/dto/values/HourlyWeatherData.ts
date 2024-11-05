514574bf87c5d02e90995c2b85dd7b8f04e851e9
seungryeol <opop0421@naver.com>
2024-11-05 16:47:11 +0900

    [feat] 필드 getter 함수 추가

--- src/dto/values/HourlyWeatherData.ts
+++ src/dto/values/HourlyWeatherData.ts
@@ -1,45 +1,74 @@
 export class HourlyWeatherDataDto {
-    private date: string;
-    private time: string;
-    private skyStatus: string;
-    private precipitationType: string;
-    private temperature: string;
-    private hourlyPrecipitation: string;
+  private date: string;
+  private time: string;
+  private skyStatus: string;
+  private precipitationType: string;
+  private temperature: string;
+  private hourlyPrecipitation: string;
 
-    constructor(date: string, time: string, skyStatus: string, precipitationType: string, temperature: string, hourlyPrecipitation: string) {
-        this.setDate(date);
-        this.setTime(time);
-        this.setSkyStatus(skyStatus);
-        this.setPrecipitationType(precipitationType);
-        this.setTemperature(temperature);
-        this.setHourlyPrecipitation(hourlyPrecipitation);
-    }
+  constructor(
+    date: string,
+    time: string,
+    skyStatus: string,
+    precipitationType: string,
+    temperature: string,
+    hourlyPrecipitation: string
+  ) {
+    this.setDate(date);
+    this.setTime(time);
+    this.setSkyStatus(skyStatus);
+    this.setPrecipitationType(precipitationType);
+    this.setTemperature(temperature);
+    this.setHourlyPrecipitation(hourlyPrecipitation);
+  }
 
-    public static of(date: string, time: string, skyStatus: string, precipitationType: string, temperature: string, hourlyPrecipitation: string) {
-        return new HourlyWeatherDataDto(date, time, skyStatus, precipitationType, temperature, hourlyPrecipitation);
-    }
+  public static of(
+    date: string,
+    time: string,
+    skyStatus: string,
+    precipitationType: string,
+    temperature: string,
+    hourlyPrecipitation: string
+  ) {
+    return new HourlyWeatherDataDto(
+      date,
+      time,
+      skyStatus,
+      precipitationType,
+      temperature,
+      hourlyPrecipitation
+    );
+  }
 
-    private setDate(date: string) {
-        this.date = date;
-    }
+  private setDate(date: string) {
+    this.date = date;
+  }
 
-    private setTime(time: string) {
-        this.time = time;
-    }
+  private setTime(time: string) {
+    this.time = time;
+  }
 
-    private setSkyStatus(skyStatus: string) {
-        this.skyStatus = skyStatus;
-    }
+  private setSkyStatus(skyStatus: string) {
+    this.skyStatus = skyStatus;
+  }
 
-    private setPrecipitationType(precipitationType: string) {
-        this.precipitationType = precipitationType;
-    }
+  private setPrecipitationType(precipitationType: string) {
+    this.precipitationType = precipitationType;
+  }
 
-    private setTemperature(temperature: string) {
-        this.temperature = temperature;
-    }
+  private setTemperature(temperature: string) {
+    this.temperature = temperature;
+  }
 
-    private setHourlyPrecipitation(hourlyPrecipitation: string) {
-        this.hourlyPrecipitation = hourlyPrecipitation;
-    }
+  private setHourlyPrecipitation(hourlyPrecipitation: string) {
+    this.hourlyPrecipitation = hourlyPrecipitation;
+  }
+
+  public getHour() {
+    return this.time;
+  }
+
+  public getPrecipitationType() {
+    return this.precipitationType;
+  }
 }
